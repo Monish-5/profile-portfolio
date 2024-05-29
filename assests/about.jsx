20677e1f221645211e68cf34e90c97f9b26890d4
Monish <164910588+Monish-5@users.noreply.github.com>
2024-05-29 14:31:31 +0530

    Create about.jsx

---
+++ assests/about.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import AboutCSS from "./About.module.css";
+import image1 from"../image/monish.jpn.jpeg";
+
+function About() {
+ 
+
+  return(
+      <>
+      <div className={AboutCSS}>
+
+          <p className={AboutCSS.para}>
+          I am Monish Fullstack Devloper <br></br>
+            I am Study BE in ECE in Hindusthan collage of engineering<br></br>
+            <h3>Mini project :Soil SMART FARMING USING IoT</h3>
+            Project Description:
+             Smart Irrigation System using the IoT to control the water moisture level and reduce the human labour and save water
+SMART FARMING USING IoT
+      <h3>Automatic Delivery Robot</h3>
+The aim of this project is to develop a automatic robot car it will be deliver the products without any human contact to the customers. This robot can be used for many delivery purposes like in any food and developing flutter framework
+app to control the robot. 
+            <h3>INTERNSHIP</h3>
+           
+            Web hosting is a service that allows organizations and individuals to post a website or web page onto the Internet. A web host, or web hosting service provider, is a business that provides the technologies and services needed for the website or webpage to be viewed in the Internet.
+          <h3>ACHIEVEMENTS</h3>
+          Successfully completion on RBBMS,DOCKER, ANGULAR AND DESIGN THINKING IN IBM course<br></br>
+Have a completion certificate in “LEARNATHON”, “MAPATHON” and “FUTURE SKILLS PRIME”.<br></br>
+Successfully completion on “Java Developer Associate JDAC-24".
+          </p>
+          
+          
+          
+          
+
+      </div>
+     
+
+      </>
+  );
+}
+
+export default About;
